a79c87b5af4522b9250b3d11778609aa0e46b529
Chris Atkins <chrisatkins55@gmail.com>
2016-03-06 23:35:30 -0500

    changing ssh key id when creating new droplets

--- build_test_env_and_make_props_file.py
+++ build_test_env_and_make_props_file.py
@@ -9,7 +9,7 @@ token = os.environ['TOKEN']
 headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + token}
 
 def create_new_server(server_name):
-    post_data={"name":server_name,"region":"nyc3","size":"1gb","image":"docker","ssh_keys":["1734092"],"backups":False,"ipv6":False,"user_data":None,"private_networking":None}
+    post_data={"name":server_name,"region":"nyc3","size":"1gb","image":"docker","ssh_keys":["1734160"],"backups":False,"ipv6":False,"user_data":None,"private_networking":None}
     new_server_response = requests.post('https://api.digitalocean.com/v2/droplets', json=post_data, headers=headers);
     print('Build server ' + server_name + ' responded with ' + str(new_server_response.status_code))
     print(new_server_response.text)
