1297ff6df1276dd7e507235ab7020a9d8295fbd4
Chris Atkins <chrisatkins55@gmail.com>
2016-03-06 22:34:50 -0500

    build test environment script now generates a file that includes the new ip address

--- build_test_env_and_make_props_file.py
+++ build_test_env_and_make_props_file.py
@@ -9,12 +9,13 @@ token = os.environ['TOKEN']
 headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + token}
 
 def create_new_server(server_name):
-    post_data={"name":server_name,"region":"nyc3","size":"1gb","image":"docker","ssh_keys":None,"backups":False,"ipv6":False,"user_data":None,"private_networking":None}
+    post_data={"name":server_name,"region":"nyc3","size":"1gb","image":"docker","ssh_keys":["1734092"],"backups":False,"ipv6":False,"user_data":None,"private_networking":None}
     new_server_response = requests.post('https://api.digitalocean.com/v2/droplets', json=post_data, headers=headers);
     print('Build server ' + server_name + ' responded with ' + str(new_server_response.status_code))
+    return str(droplet['networks']['v4'][0]['ip_address'])
 
 
-create_new_server(server_name='TEST-ENV-RECIPE-SERVICE')
+test_recipe_ip = create_new_server(server_name='TEST-ENV-RECIPE-SERVICE')
 # create_new_server(server_name='TEST-ENV-RECIPE-WEB')
 
 
@@ -31,9 +32,12 @@ while all_servers_ready == False:
         
 print('All droplets are ready to go!')
 for droplet in getDropletsResponse.json()['droplets']:
-    print(str(droplet['name']) + ' | ' + str(droplet['id']))
+    print(str(droplet['name']) + ' | ' + str(droplet['id']) + ' | ' + str(droplet['networks']['v4'][0]['ip_address']) + ' ' + str(droplet['networks']['v4'][0]['type']))
         
-
-
+file = open('env.props', 'w')
+file.write('TEST=hi, its me!\n')
+file.write('SSH_HOSTNAME=' + test_recipe_ip + '\n')
+file.close()
+print('done creating file')
 
 print('Done building test environments')
