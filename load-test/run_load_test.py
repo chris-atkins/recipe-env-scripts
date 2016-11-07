90347a44b579c518b6418c7a34d222975a34f588
Chris Atkins <chrisatkins55@gmail.com>
2016-11-06 22:03:46 -0500

    adding a simple api call in the load test to prove out things are working

--- load-test/run_load_test.py
+++ load-test/run_load_test.py
@@ -1,12 +1,16 @@
 #!
 
-import getopt, sys, time
+import getopt, sys, time, requests
 
 def main():
     num_users = str(sys.argv[1])
     web_ip = str(sys.argv[2])
     print('Running Test against ' + web_ip + ' with ' + num_users + ' user thread(s).')
-    time.sleep(30)
+    url = 'http://' + web_ip + ':8000/api/recipe'
+    for i in xrange(10):
+        time.sleep(3)
+        requests.get(url, headers={})
+
     print('Done Running Test')
 
 if __name__ == "__main__":
