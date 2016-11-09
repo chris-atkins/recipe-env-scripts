b79fc2d24e2ba4e74889a36dfbe51af5f2f7e824
Chris Atkins <chrisatkins55@gmail.com>
2016-11-08 21:07:03 -0500

    removing logs from the load test

--- load-test/run_load_test.py
+++ load-test/run_load_test.py
@@ -13,9 +13,9 @@ class SingleUser(Thread):
         self.base_url = 'http://' + web_ip + ':8000'
 
     def run(self):
-        self.runSingleUserForever()
-        # while(True):
-        #     self.runSingleUserForever()
+        # self.runSingleUserForever()
+        while(True):
+            self.runSingleUserForever()
 
     def random_string(self, length):
         return ''.join(random.choice(string.ascii_letters) for i in range(length))
@@ -76,117 +76,117 @@ class SingleUser(Thread):
 
     def header(self, user_id=None):
         if user_id is None:
-            print('NONE')
+            # print('NONE')
             return {'Content-Type': 'application/json'}
         else:
-            print('user_id: ' + user_id)
+            # print('user_id: ' + user_id)
             return {'Content-Type': 'application/json', 'RequestingUser': user_id}
 
 
     def getRecipeList(self, user_id):
         try:
-            print('Thread ' + str(self.thread_id) + ': GET Recipe List')
+            # print('Thread ' + str(self.thread_id) + ': GET Recipe List')
             response = requests.get(self.base_url + '/api/recipe', headers=self.header(user_id=user_id))
-            print(str(response.status_code) + '\n')
+            # print(str(response.status_code) + '\n')
         except Exception as e:
-            print('Exception', e)
+            # print('Exception', e)
             return
 
 
     def getUserRecipeBook(self, user_id):
         try:
-            print('Thread ' + str(self.thread_id) + ': GET Recipe Book')
+            # print('Thread ' + str(self.thread_id) + ': GET Recipe Book')
             response = requests.get(self.base_url + '/api/user/' + user_id + '/recipe-book', headers=self.header(user_id=user_id))
-            print(str(response.status_code) + '\n')
+            # print(str(response.status_code) + '\n')
         except Exception as e:
-            print('Exception', e)
+            # print('Exception', e)
             return
 
 
     def getUserByEmail(self, email):
         try:
-            print('Thread ' + str(self.thread_id) + ': GET User by email')
+            # print('Thread ' + str(self.thread_id) + ': GET User by email')
             response = requests.get(self.base_url + '/api/user?email=' + email, headers=self.header())
-            print(str(response.status_code) + '\n')
+            # print(str(response.status_code) + '\n')
         except Exception as e:
-            print('Exception', e)
+            # print('Exception', e)
             return
 
 
     def postUser(self, users, emails):
         try:
-            print('Thread ' + str(self.thread_id) + ': POST User')
+            # print('Thread ' + str(self.thread_id) + ': POST User')
             post_data = {"userName": self.random_string(self.user_name_length()), "userEmail": self.random_string(self.email_length())}
             response = requests.post(self.base_url + '/api/user', json=post_data, headers=self.header())
             users.append(response.json()['userId'])
             emails.append(response.json()['userEmail'])
-            print(str(response.status_code) + '\n')
+            # print(str(response.status_code) + '\n')
         except Exception as e:
-            print('Exception', e)
+            # print('Exception', e)
             return
 
 
     def postRecipe(self, user_id, recipes):
         try:
-            print('Thread ' + str(self.thread_id) + ': POST Recipe')
+            # print('Thread ' + str(self.thread_id) + ': POST Recipe')
             post_data = {"recipeName": self.random_string(self.recipe_name_length()), "recipeContent": self.random_string(self.recipe_content_length())}
             response = requests.post(self.base_url + '/api/recipe', json=post_data, headers=self.header(user_id=user_id))
             recipes.append(response.json()['recipeId'])
-            print(str(response.status_code) + '\n')
+            # print(str(response.status_code) + '\n')
         except Exception as e:
-            print('Exception', e)
+            # print('Exception', e)
             return
 
 
     def putRecipe(self, recipe_id, user_id):
         try:
-            print('Thread ' + str(self.thread_id) + ': PUT Recipe')
+            # print('Thread ' + str(self.thread_id) + ': PUT Recipe')
             put_data = {"recipeId": recipe_id, "recipeName": self.random_string(self.recipe_name_length()), "recipeContent": self.random_string(self.recipe_content_length())}
             response = requests.put(self.base_url + '/api/recipe/' + recipe_id, json=put_data, headers=self.header(user_id=user_id))
-            print(str(response.status_code) + '\n')
+            # print(str(response.status_code) + '\n')
         except Exception as e:
-            print('Exception', e)
+            # print('Exception', e)
             return
 
 
     def getRecipe(self, recipe_id, user_id):
         try:
-            print('Thread ' + str(self.thread_id) + ': GET Recipe')
+            # print('Thread ' + str(self.thread_id) + ': GET Recipe')
             response = requests.get(self.base_url + '/api/recipe/' + recipe_id, headers=self.header(user_id=user_id))
-            print(str(response.status_code) + '\n')
+            # print(str(response.status_code) + '\n')
         except Exception as e:
-            print('Exception', e)
+            # print('Exception', e)
             return
 
 
     def getUserById(self, user_id):
         try:
-            print('Thread ' + str(self.thread_id) + ': GET User')
+            # print('Thread ' + str(self.thread_id) + ': GET User')
             response = requests.get(self.base_url + '/api/user/' + user_id, headers=self.header())
-            print(str(response.status_code) + '\n')
+            # print(str(response.status_code) + '\n')
         except Exception as e:
-            print('Exception', e)
+            # print('Exception', e)
             return
 
 
     def postUserRecipeBook(self, user_id, recipe_id):
         try:
-            print('Thread ' + str(self.thread_id) + ': POST to Recipe Book')
+            # print('Thread ' + str(self.thread_id) + ': POST to Recipe Book')
             post_data = {"recipeId": recipe_id}
             response = requests.post(self.base_url + '/api/user/' + user_id + '/recipe-book', json=post_data, headers=self.header(user_id=user_id))
-            print(str(response.status_code) + '\n')
+            # print(str(response.status_code) + '\n')
         except Exception as e:
-            print("Exception", e)
+            # print("Exception", e)
             return
 
 
     def deleteRecipeFromRecipeBook(self, user_id, recipe_id):
         try:
-            print('Thread ' + str(self.thread_id) + ': DELETE from Recipe Book')
+            # print('Thread ' + str(self.thread_id) + ': DELETE from Recipe Book')
             response = requests.delete(self.base_url + '/api/user/' + user_id + '/recipe-book/' + recipe_id, headers=self.header(user_id=user_id))
-            print(str(response.status_code) + '\n')
+            # print(str(response.status_code) + '\n')
         except Exception as e:
-            print("Exception", e)
+            # print("Exception", e)
             return
 
 
@@ -331,15 +331,15 @@ class SingleUser(Thread):
 
         self.putRecipe(self.random_recipe(recipes), self.random_user_id(users))
 
-        print(str(self.thread_id) + ' userIds: ')
-        for user in users:
-            print(str(self.thread_id) + '      ' + user)
-        print(str(self.thread_id) + ' emails: ')
-        for email in emails:
-            print(str(self.thread_id) + '      ' + email)
-        print(str(self.thread_id) + ' recipeIds: ')
-        for recipe in recipes:
-            print(str(self.thread_id) + '      ' + recipe)
+        # print(str(self.thread_id) + ' userIds: ')
+        # for user in users:
+            # print(str(self.thread_id) + '      ' + user)
+        # print(str(self.thread_id) + ' emails: ')
+        # for email in emails:
+            # print(str(self.thread_id) + '      ' + email)
+        # print(str(self.thread_id) + ' recipeIds: ')
+        # for recipe in recipes:
+            # print(str(self.thread_id) + '      ' + recipe)
 
 
 
@@ -348,7 +348,7 @@ if __name__ == "__main__":
     num_users = int(sys.argv[1])
     web_ip = str(sys.argv[2])
     time_between_new_users = int(sys.argv[3])
-    print('Running Test against ' + web_ip + ' with ' + str(num_users) + ' user thread(s), with a new user starting approximately every ' + str(time_between_new_users) + ' seconds.')
+    # print('Running Test against ' + web_ip + ' with ' + str(num_users) + ' user thread(s), with a new user starting approximately every ' + str(time_between_new_users) + ' seconds.')
 
     userThreads = []
     for i in range(num_users):
@@ -360,5 +360,5 @@ if __name__ == "__main__":
     for userThread in userThreads:
         userThread.join()
 
-    print('Done running tests')
+    # print('Done running tests')
 
